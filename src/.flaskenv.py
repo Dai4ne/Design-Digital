8046411036ba0910a809c2e6d5baf8e62bd16cc4
Daiane <daianeksilva07@gmail.com>
2026-05-31 20:48:30 -0300

    persistência de dados com form e mensagem de envio

--- src/.flaskenv.py
+++
@@ -1,1 +0,0 @@
-FLASK_APP=Desafio01
--- src/routes.py
+++ src/routes.py
@@ -1,21 +1,63 @@
 import sys
 import os
+import pymysql 
+from flask import Flask, render_template, request, redirect, url_for
+from dotenv import load_dotenv
+
+#Para o python encontrar as pastas corretamente
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
-from flask import Flask, render_template
+#Carrega as config do .env 
+load_dotenv()
 
 app = Flask(__name__, 
             template_folder='../src/templates', 
             static_folder='../src/static')
+
+#Função para pegar as config de conexão com o banco do .env
+def conexao_db():
+    return pymysql.connect(
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME'),
+        port=3306,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/contatos')
+@app.route('/contatos', methods=['GET', 'POST'])
 def contatos():
+    if request.method == 'POST':
+        #Informações que foram digitadas no form
+        nome_form = request.form.get('nome')
+        email_form = request.form.get('email')
+        assunto_form = request.form.get('assunto')
+        descricao_form = request.form.get('descricao')
+
+        conexao = conexao_db()
+        try:
+            with conexao.cursor() as cursor:
+                sql = '''
+                    INSERT INTO contatos (nome, email, assunto, descricao) VALUES (%s, %s, %s, %s)
+                '''
+                cursor.execute(sql, (nome_form, email_form, assunto_form, descricao_form))
+
+            conexao.commit() #Grava na AWS
+            return redirect(url_for('contatos')) #Recarrega a página limpa
+        
+        except Exception as e:
+            return f"Erro ao salvar as informações no banco {e}."
+        finally:
+            conexao.close()
+
     return render_template('contatos.html')
+
 
 @app.route('/quem')
 def quem():
